6113aa93870545889dd2565c05fb2aa13f98205c
Rahul Sharma <67586619+rahulsharma-ux@users.noreply.github.com>
2021-04-30 22:41:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var css = document.querySelector("h3");
+var c1 = document.querySelector(".c1");
+var c2 = document.querySelector(".c2");
+var body = document.getElementById("bg");
+
+function color() {
+    body.style.background = "linear-gradient(to right, " + c1.value + ", " + c2.value + ")";
+    css.textContent = body.style.background + ";";
+}
+
+c1.addEventListener("input", color);
+
+c2.addEventListener("input", color);
